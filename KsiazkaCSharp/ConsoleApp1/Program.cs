8534b4ec46c0707b9f07fcfe0cdb898bffed655c
Hubert Kuszyński <hkuszynski@gmail.com>
2019-05-24 16:07:50 +0200

    Zadanie ukonczone, cwiczenia komentarzy

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -6,10 +6,20 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    /* To jest główna klasa aplikacji
+     */
+    /// <summary>
+    /// To jest główna klasa aplikacji
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// glowna metoda i cialo prog.
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
-        }
+            Console.WriteLine("Kurwa mac jebać!");
+        }/* Program wyswietli ci na pulpicie ten tekst ktory jest wpisany we WriteLine(); */
     }
 }
