51debe0eb87c57741d93400314de74dc93a5c5e3
Hubert Kuszyński <hkuszynski@gmail.com>
2019-05-26 12:22:05 +0200

    zadania od 7.1 do 7.4

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -10,6 +10,27 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            /* int a = 3, b = 6, c = 5;
+             int wynik = a % b % c;
+             Console.WriteLine(wynik);
+             Console.ReadKey() */ // Zadanie 7.1
+            /* int liczba = 256;
+            int liczba1 = liczba >> 1;
+            Console.WriteLine(liczba1);
+            int liczba2 = liczba << 1;
+            Console.WriteLine(liczba2);
+            Console.ReadKey(); */ // zadnie 7,2
+            /* int liczba1 = 52, liczba2 = 22;
+            int wynik = liczba1 | liczba2;
+            Console.WriteLine(wynik);
+            wynik = liczba1 & liczba2;
+            Console.WriteLine(wynik);
+            Console.ReadKey(); */ // zadanie 7.3
+            /* int liczba = 55;
+            liczba ^= 22;
+            Console.WriteLine(liczba);
+            Console.ReadKey(); */ // zadanie 7.4
+           
         }
     }
 }
