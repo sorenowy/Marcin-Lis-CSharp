3c6a95d4fa010475045818727c6cb746cb864803
Hubert Kuszyński <hkuszynski@gmail.com>
2019-05-30 14:54:40 +0200

    Ostatnie zadanie do analizy :D

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -54,11 +54,60 @@ namespace ConsoleApp1
                     liczba--;
                     continue;
                 
-            } */ // ZADANIE 10.3
-            int liczba = -100;
-            while (liczba <= 100)
+            } */ // ZADANIE 11.3
+                 /*
+                 int liczba = -100; 
+                 while (liczba <= 100)
+                 {
+                     if (liczba % 4 == 0)
+                     {
+                         if (liczba % 8 != 0)
+                         {
+                             if (liczba % 10 != 0)
+                             {
+                                 Console.WriteLine($"Liczba {liczba} jest podzielna przez 4 ale nie podzielna przez 8 i 10");
+                                 liczba++;
+                             }
+                             else
+                                 liczba++;
+                             continue;
+                         }
+                         else
+                         liczba++;
+                         continue;
+                     }
+                     else
+                     liczba++;
+                     continue;
+                 } */ // ZADANIE 11.4
+
+            /* 
+            for (int i=1;i<=20;i++)
             {
-                if(liczba
+                if (i % 2 == 0)
+                Console.Write("{0} ", i);
+            }
+             */ // ZADANIE 11.5
+            
+            for (int i = 20; i >= 1; i--)
+            {
+                int liczba = 20;
+                for (int j = 20; j >= 1; j--)
+                {
+                    for (int z =20;z>=1;z--)
+                    {
+                        Console.Write(liczba + " ");
+                    }
+                    Console.WriteLine();
+                    liczba--;
+                }
+                if (i == 1)
+                {
+                    Console.WriteLine();
+                    Console.WriteLine("20");
+                }                    
+                else
+                    Console.WriteLine(); 
             }
             Console.ReadKey();
         }
