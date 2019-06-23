ab1c793bca46934600946db0c1176e2b95e5d651
Hubert Kuszyński <hkuszynski@gmail.com>
2019-06-23 16:39:29 +0200

    Zadanie 24.5 do wykonania

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -8,8 +8,36 @@ namespace ConsoleApp1
 {
     class Program
     {
+        public void Metoda(string tekst)
+        {
+            if ()
+        }
         static void Main(string[] args)
         {
+            Console.WriteLine("Kurw mac"); // Zadanie 24.1
+            Console.Write("\u004B\u0055\u0052\u0057\u0041!"); // Zadanie 24.2
+            Console.WriteLine();
+            string str = "Przykładowy tekst"; // Zadanie 24.3
+            for (int i = str.Length; i > 0; i--)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    Console.Write(str[i - 1]);
+                }
+                Console.WriteLine();
+            }
+            string tekst = "ABCDEFGHIJKLMNOPRSTUWXYZabcdefghijklmnoprstuwxyz"; // Zadanie 24.4 
+            Console.WriteLine("Alfabet w systemie szesnastkowym:");
+            for (int i = 0; i < tekst.Length; i++)
+            {
+                Console.Write("{0} = {1:X} |",tekst[i],Convert.ToInt32(tekst[i]));
+                if (i %10==0)
+                {
+                    Console.WriteLine("\n");
+                }
+            }
+
+            Console.ReadKey();
         }
     }
 }
