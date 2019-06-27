2ef4ff01a3c15a505c868810e4cc7ea6b2319c5b
Hubert Kuszyński <hkuszynski@gmail.com>
2019-06-27 13:23:35 +0200

    Commit :)

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace ConsoleApp1
 {
@@ -10,6 +11,29 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
+            string katalog;
+            string nazwa;
+            Console.WriteLine("Podaj nazwę katalogu:");
+            katalog = Console.ReadLine();
+            
+            Console.WriteLine("Ile podkatalogow chcesz utworzyc??");
+            int liczba = int.Parse(Console.ReadLine());
+            DirectoryInfo di = new DirectoryInfo(katalog);
+            for (int i = 0; i < liczba; i++)
+            {
+                Console.WriteLine("Podaj nazwę subkatalogu dla {0} podkatalogu", i + 1);
+                di.CreateSubdirectory(Console.ReadLine());
+            }
+            Console.WriteLine("Podaj nazwe w celu wyszukania, użyj * aby zawężyć pole wyszukiwania:");
+            nazwa = Console.ReadLine();
+            DirectoryInfo[] dire = di.GetDirectories(nazwa);
+            foreach (DirectoryInfo dir in dire)
+            {
+                Console.WriteLine(dir.Name);
+            }
+            Console.WriteLine("Wyszukiwany jest katalog {0} oraz pliki o haśle {1}", katalog, nazwa);
+
+            Console.ReadKey();
         }
     }
 }
