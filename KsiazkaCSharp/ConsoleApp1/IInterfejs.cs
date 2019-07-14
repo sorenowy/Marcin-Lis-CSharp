3004fa060617e8cece90765e488cc96abb796a96
Hubert Kuszyński <hkuszynski@gmail.com>
2019-07-14 12:31:45 +0200

    Zadania 31.3 do 31.5

---
+++ KsiazkaCSharp/ConsoleApp1/IInterfejs.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    interface IInterfejs
+    {
+        int x { get; set; }
+        double y { get; set; }
+    }
+    interface IPunkt3D : IInterfejs
+    {
+        int z { get; set; }
+    }
+    class Klasa : IInterfejs
+    {
+        private int intx;
+        private double doubley;
+        public int x
+        {
+            get
+            {
+                return intx;
+            }
+            set
+            {
+                intx = value;
+            }
+        }
+        public double y
+        {
+            get
+            {
+                return doubley;
+            }
+            set
+            {
+                doubley = value;
+            }
+        }
+    }
+    class Punkt3D : Klasa, IPunkt3D
+    {
+        private int punktZ;
+        public int z
+        {
+            get
+            {
+                return punktZ;
+            }
+            set
+            {
+                punktZ = value;
+            }
+        }
+    }
+}
--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -32,10 +32,38 @@ namespace ConsoleApp1
             Console.ReadKey();
             Shape sh1 = new Shape("kurwa");
             Circle c1 = new Circle();
+            Triangle t1 = new Triangle();
+            Rectangle r1 = new Rectangle();
             sh1.Draw();
             c1.Draw();
             r1.Draw();
             t1.Draw();
+            Console.ReadKey();
+            Klasa k1 = new Klasa();
+            k1.x = 6;
+            k1.y = 7.5;
+            Console.WriteLine(k1.y);
+            Console.WriteLine(k1.x);
+            Console.ReadKey();
+            Console.Clear(); 
+            // -------------------- ZADANIE 31.5 //
+            Punkt3D punkt3d1 = new Punkt3D();
+            punkt3d1.x = 10;
+            punkt3d1.y = 20;
+            punkt3d1.z = 30;
+            Console.WriteLine("Punkt3D1");
+            Console.WriteLine("x = " + punkt3d1.x);
+            Console.WriteLine("y = " + punkt3d1.y);
+            Console.WriteLine("z = " + punkt3d1.z);
+            Klasa punkt1 = (Klasa)punkt3d1;
+            Console.WriteLine("Punkt1");
+            Console.WriteLine("x = " + punkt1.x);
+            Console.WriteLine("y = " + punkt1.y);
+            Punkt3D punkt3d2 = (Punkt3D)punkt1;
+            Console.WriteLine("Punkt3D2");
+            Console.WriteLine("x = " + punkt3d2.x);
+            Console.WriteLine("y = " + punkt3d2.y);
+            Console.WriteLine("z = " + punkt3d2.z);
             Console.ReadKey();
         }
     }
