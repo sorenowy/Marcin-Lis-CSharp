cd62d79fa33248ae6f3b1e10285312974308d538
Hubert Kuszyński <hkuszynski@gmail.com>
2019-07-19 15:18:40 +0200

    Zadanie 34.1 i 34.2

--- KsiazkaCSharp/ConsoleApp1/Program.cs
+++ KsiazkaCSharp/ConsoleApp1/Program.cs
@@ -6,10 +6,76 @@ using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
+    class TablicaInt
+    {
+        private int[] tab;
+        public TablicaInt(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException("rozmiar tablicy < 0");
+            }
+            tab = new int[size];
+        }
+        public int Get(int index)
+        {
+            if(index>=tab.Length || index < 0)
+            {
+                throw new IndexOutOfRangeException("Index = " + index);
+            }
+            else
+            {
+                return tab[index];
+            }
+        }
+        public void Set(int index,int value)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("Index = " + index);
+            }
+            if (index >= tab.Length)
+            {
+                Resize(index + 1);
+            }
+            tab[index] = value;
+        }
+        protected void Resize(int size)
+        {
+            int[] newTab = new int[size];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                newTab[i] = tab[i];
+            }
+            tab = newTab;
+        }
+        public int Length
+        {
+            get
+            {
+                return tab.Length;
+            }
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
+            TablicaInt tablica = new TablicaInt(1);
+            tablica.Set(100, 5);
+            for (int i = 1; i < tablica.Length; i++)
+            {
+                Console.WriteLine($"Tablica {i} zawiera {tablica.Get(i)}");
+            }
+            Tablica rectangles = new Tablica(3);
+            rectangles.Set(3, new Triangle());
+            rectangles.Set(4, new Triangle());
+            rectangles.Set(2, new Rectangle());
+            for (int i = 0; i < tablica.Length; i++)
+            {
+                ((Rectangle)rectangles.Get(i)).Diagonal();
+            }
+            Console.ReadKey();
         }
     }
 }
---
+++ KsiazkaCSharp/ConsoleApp1/Tablica.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Tablica
+    {
+        private Object[] tab;
+        public Tablica(int size)
+        {
+            if (size < 0)
+            {
+                throw new ArgumentOutOfRangeException("Size<0");
+            }
+            else
+            {
+                tab = new Object[size];
+            }
+        }
+        public Object Get(int index)
+        {
+            if (index >= tab.Length || index < 0)
+            {
+                throw new IndexOutOfRangeException("Index = "+index);
+            }
+            else
+            {
+                return tab[index];
+            }
+        }
+        public void Set(int index,Object obj)
+        {
+            if (index < 0)
+            {
+                throw new IndexOutOfRangeException("Index = "+index);
+            }
+            else if (index >= tab.Length)
+            {
+                Resize(index + 1);
+            }
+            else
+            {
+                tab[index] = obj;
+            }
+        }
+        protected void Resize(int size)
+        {
+            Object[] newTab = new Object[size];
+            for (int i = 0; i < tab.Length; i++)
+            {
+                newTab[i] = tab[i];
+            }
+            tab = newTab;
+        }
+        public int Length
+        {
+            get
+            {
+                return tab.Length;
+            }
+        }
+    }
+}
